ec913bd3a28b200ab117a0afaa8bace71394d3b3
Miri Radunsky <miriradunsky@gmail.com>
2025-05-02 00:39:12 +0300

    data base

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad561c82ca19f177400ab8ee0c034fb81ff33291")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7134c32d08994fedc50d6b54d8da7dbf2c901d41")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
