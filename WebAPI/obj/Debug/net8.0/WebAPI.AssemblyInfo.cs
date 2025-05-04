a1c188fd920780c68fbfecca9dc90e6cd3529adc
Miri Radunsky <miriradunsky@gmail.com>
2025-05-04 16:01:25 +0300

    delete bl

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad561c82ca19f177400ab8ee0c034fb81ff33291")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e5494f4c55e469c3bf119d700d47bf57a73897c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
